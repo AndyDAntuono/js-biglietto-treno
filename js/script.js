e281778a4fca7bf5ce0f25c750a7cafe48912f64
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-06-25 18:09:02 +0200

    [UPD] added age prompt

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ const km = prompt("Quanti km devi percorrere?")
 
 //devo applicare 0.21 € per ogni chilometro percorso dall'utente
 const ticketPrice = km * 0.21
+
+//chiedo all'utente la sua età
+const age = prompt("Quanti anni hai?")
