dbcfb3f7d78a2068629f60c67aff72ece51f8971
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-06-25 18:05:55 +0200

    [UPD] added const ticketPrice

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 //chiedo all'utente quanta distanza deve percorrere
 const km = prompt("Quanti km devi percorrere?")
+
+//devo applicare 0.21 € per ogni chilometro percorso dall'utente
+const ticketPrice = km * 0.21
