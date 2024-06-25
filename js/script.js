4c71dfa71cb1b13f85c579212b9602a7e858f238
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-06-25 18:40:04 +0200

    [UPD] added const discountPrice

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const km = prompt("Quanti km devi percorrere?")
 
 //devo applicare 0.21 € per ogni chilometro percorso dall'utente
-const ticketPrice = km * 0.21
+const fullPrice = km * 0.21
 
 //chiedo all'utente la sua età
 const age = prompt("Quanti anni hai?")
@@ -20,3 +20,6 @@ if(age < 18 ) {
 else if(age > 65) {
     discount = 40;
 }
+
+//calcolo il prezzo finale
+const discountPrice = fullPrice - (fullPrice * discount / 100)
